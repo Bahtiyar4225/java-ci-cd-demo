f53c71f2963992d9d8f605e9ae370e99e36f2fd2
Bahtiyar Kurt <bahtiyarkurt42@gmail.com>
2026-01-14 20:53:40 +0300

    ilk kurulum: proje baslatildi

---
+++ src/main/java/com/ci_cd_ogreniyorum/demo_app/DemoAppApplication.java
@@ -0,0 +1,13 @@
+package com.ci_cd_ogreniyorum.demo_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ci_cd_ogreniyorum/demo_app/DemoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ci_cd_ogreniyorum.demo_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
